075c6e4d8e8a3b0084ad2cfd594c0d5a28476461
Kim Aragon Escobar <kim.ae09@gmail.com>
2018-09-15 15:01:39 -0300

    Initializing project.

---
+++ src/main/java/br/com/kimae/salesanalyzer/SalesAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package br.com.kimae.salesanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesAnalyzerApplication.class, args);
+	}
+}
